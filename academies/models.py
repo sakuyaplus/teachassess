93318675c0942db3d0a81b63b3b93554e5af7beb
sakuyaplus <2358537243@qq.com>
2021-07-25 10:05:23 +0800

    教师检索

--- academies/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Academy(models.Model):
-    
-    academy_name=models.CharField(max_length=50) #学院名
-  
-    def __str__(self):
-        return str(self.academy_name)
--- teachers/views.py
+++ teachers/views.py
@@ -1,18 +1,46 @@
 from tcomments.models import TComment
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
-from academies.models import Academy
+from django.db.models import Q
 from .models import Teacher
+from .choices import academy_choices,rank_choices
 # Create your views here.
 def index(request):
     listings = Teacher.objects.all().filter(show=True)
+
+    if 'q' in request.GET:
+        keywords=request.GET['q']
+     
+        if keywords:
+            listings = listings.filter(
+                Q(name__icontains=keywords)|Q(contact__icontains=keywords)|Q(studyexp__icontains=keywords)
+                |Q(researchfield__icontains=keywords)
+            )
+
+    if 'academy' in request.GET:
+        academy=request.GET['academy']
+      
+        if academy !='全部':
+            listings = listings.filter(
+                dept__iexact=academy
+            )
+
+    if 'rank' in request.GET:
+        rank=request.GET['rank']
+
+        if rank !='全部':
+            listings = listings.filter(
+                rank__icontains=rank
+            )   
+
     paginator=Paginator(listings,6)
     page=request.GET.get('page')
     paged_listings=paginator.get_page(page)
-    academies=Academy.objects.all()
     context={
         'listings': paged_listings,
-        'academies': academies,
+        'academy_choices':  academy_choices,
+        'rank_choices': rank_choices,
+        'values': request.GET
     }
 
     return render(request, 'teachers/teachers.html',context)
